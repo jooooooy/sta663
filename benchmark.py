1dad0eb56859f89e29f540f36f80201b4acbd4fd
ag400 <31577419+ag400@users.noreply.github.com>
2018-04-26 18:03:21 -0400

    Update and rename benchmark to benchmark.py

---
+++ benchmark.py
@@ -0,0 +1,7 @@
+
+########################################################################################
+# Code for implementing the Toni et al. alogrithm with predefined levels of
+# approximation.
+#
+# Implementation for the Abakiliki data
+########################################################################################
