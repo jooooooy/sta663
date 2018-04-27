f97fc8d3e0dc2322ff5ccfc9785519e74abf6289
ag400 <31577419+ag400@users.noreply.github.com>
2018-04-27 00:01:09 -0400

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -47,3 +47,76 @@ print('running time is:', tendC-tstartC)
 #################################################################################################################
 # ABC code for running ABC using Toni et al. SMC
 #################################################################################################################
+SeattleA = np.array([[15,12,4],[11,17,4],[0,21,4],[0,0,5]])
+Tecumseh1 = np.zeros((8,7))
+Tecumseh1[0,0:5] = np.array((66 ,87 ,  25  , 22   , 4 ))
+Tecumseh1[1,0:5] = np.array((13 ,14 ,  15  ,  9   , 4 ))
+Tecumseh1[2,1:6] = np.array((4 ,   4  ,  9  ,  2 ,   1 ))
+Tecumseh1[3,2:7] = np.array(( 4   , 3   , 1  ,  1 ,   1))
+Tecumseh1[4,3:5] = np.array(( 1,1))
+
+tstartC = time.time()
+
+# Set dataset: SeattleA, SeattleB, Tecumseh1, Tecumseh2
+DATA = Tecumseh1
+
+# Process data
+proc = process(DATA)
+hsA, isA, colA, rowA, xA, N = proc
+# Code for running coupled algorithm and selecting which simulations to keep.
+#
+# Set thresholds
+Eps1 = [100, 70, 50]
+Eps2 = [10, 6, 4]
+
+TT = len(Eps1)
+EpsM = np.vstack((Eps1,Eps2)).T
+
+# Set infectious period (k) and  number of iterations (run)
+k = 0
+run = 1000
+
+# Initial run
+epss = EpsM[0,:]
+
+OUTP = House_van(DATA, EpsM[0,:], k, run)
+OUT = OUTP['OUTPUT']
+simT = OUTP['simcount']
+
+OL = np.zeros((run, 3))
+OL[:, 0:2] = OUT
+OL[:, 2] = np.repeat(1, run)
+
+# Subsequent runs using importance sampling
+if TT > 1:
+    for t in range(1, TT):
+        OUTP = House_imp(DATA,EpsM[t,],k,run,OL)
+        OL = OUTP['OUTPUT']
+        simT+=OUTP['simcount']        
+
+OUT = OL
+
+
+# Moment calculations for SMC-ABC output
+weiA = np.sum(OUT[:,2])
+meanG = np.sum(OUT[:,0]*OUT[:,2])/weiA
+sdG = math.sqrt(np.sum(OUT[:,0]**2*OUT[:,2])/weiA - meanG**2)
+meanL = np.sum(OUT[:,1]*OUT[:,2])/weiA
+sdL=math.sqrt(np.sum(OUT[:,1]**2*OUT[:,2])/weiA - meanL**2)
+
+# Moment calculations for transformed variables
+
+meanqG = np.sum(math.exp(-OUT[:,0]*xA/N)*OUT[:,2])/weiA
+sdqG = math.sqrt(np.sum(math.exp(-2*OUT[:,0]*xA/N)*OUT[:,2])/weiA-meanqG**2)
+meanqL = np.sum(math.exp(-OUT[:,1])*OUT[:,2])/weiA
+sdqL = math.sqrt(np.sum(math.exp(-2*OUT[:,1])*OUT[:,2])/weiA-meanqL**2)
+
+# Summarise results
+print("Summarise results", [meanG, sdG, meanL, sdL])
+
+# Transformed parameters.
+print("Transformed parameters.", [meanqG, sdqG, meanqL, sdqL])
+print(simT)
+
+tendC = time.time()
+print('running time is', tendC-tstartC)
