e631598bfa11bb6a572abf4ce86ae6fc66e2f68f
ag400 <31577419+ag400@users.noreply.github.com>
2018-04-27 23:57:21 -0400

    Update optimization_code.py

--- optimization_code.py
+++ optimization_code.py
@@ -18,5 +18,60 @@ def thres_numba(n):
         
     return thres
     
- 
+def epigammaF_numba(n, m, run, k):
+    """Simulates epidemics with a Gamma(k,k) infectious period.""" 
+    # output gives the set of lambda parameters consistent with the data (m out of n infected)
+    # Only successful simulations kept 
+    # k=0 constant infectious period
+    output = np.zeros((run, 2))
+    count, j = 0, 0
+    while j<run:
+        t = thres_numba(n)
+        count+=1
+        if k>0:
+            q = np.random.gamma(k, 1/k, n)
+        if k == 0:
+            q = np.repeat(1.0, n)
+        y = []
+        for i in range(1, n):
+            y.append(t[i-1]/np.sum(q[0:i]))
+        q = max(y[0:(m-1)])
+        if q<y[m-1]:           
+            output[j,:] = np.array([q, y[m-1]])
+            j+=1
+        
+    return output
+
+@jit
+def bernSIR_numba(n, beta, gamma, p):
+    t, count = 0, 0
+    # Matrix of edges - non-symmetric but ok because we will only use MAT[i,j] if
+    # i infected before j or MAT[j,i] if j infected before i.
+    MAT = np.random.binomial(1, p, n**2).reshape((n, n))
+    rowM = np.sum(MAT, axis = 1)
+    # Set individual 1 infectious and everybody else susceptible.
+    I = np.zeros(n)
+    I[0] = 1
+    output = np.zeros(n) # Recovery times
+    while np.sum(I == 1) > 0:
+        rec = np.sum(I == 1)
+        infe = np.sum(rowM[I == 1])
+        t+= np.random.exponential(1/gamma*rec+beta*infe, 1)
+        u = np.random.uniform(0, 1, 1)
+        if u <= beta*infe/(gamma*rec+beta*infe):
+            S = np.zeros(n)
+            S[I==1] = rowM[I==1]
+            K = np.random.choice(np.arange(n), 1, p = S/S.sum(), replace = True)
+            J = np.random.choice(np.arange(n), 1, p = MAT[K,].ravel()/MAT[K,].ravel().sum(), replace = True)
+            if I[J] == 0:
+                I[J] = 1
+        else:
+            S = np.zeros(n)
+            S[I==1] = 1
+            K = np.random.choice(n, 1, p = S/S.sum(), replace = True)
+            I[K] = 2
+            count+=1
+            output[count-1]=t
+            
+    return output, count   
 
