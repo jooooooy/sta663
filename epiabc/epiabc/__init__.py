a677c82675eabdb8d6bfe0eabdeae1be92fc0df2
ag400 <31577419+ag400@users.noreply.github.com>
2018-04-30 03:41:15 -0400

    Update __init__.py

--- epiabc/epiabc/__init__.py
+++ epiabc/epiabc/__init__.py
@@ -743,3 +743,37 @@ def abcSIR_discrete(obs_data, N, T, epsilon, prior_param, samples):
             post_samples[i-1,] = np.array((Lambda, gamma))
             
         return post_samples
+    
+   
+@jit
+def thres_numba(n):
+    thres = np.zeros(n-1)
+    thres[0] = np.random.exponential(n/(n-1), 1)[0]
+    for i in range(1, n-1):
+        thres[i] = thres[i-1] + np.random.exponential(n/(n-i-1), 1)[0]
+        
+    return thres
+
+def epigammaF_numba(n, m, run, k):
+    """Simulates epidemics with a Gamma(k,k) infectious period.""" 
+    # output gives the set of lambda parameters consistent with the data (m out of n infected)
+    # Only successful simulations kept 
+    # k=0 constant infectious period
+    output = np.zeros((run, 2))
+    count, j = 0, 0
+    while j<run:
+        t = thres_numba(n)
+        count+=1
+        if k>0:
+            q = np.random.gamma(k, 1/k, n)
+        if k == 0:
+            q = np.repeat(1.0, n)
+        y = []
+        for i in range(1, n):
+            y.append(t[i-1]/np.sum(q[0:i]))
+        q = max(y[0:(m-1)])
+        if q<y[m-1]:           
+            output[j,:] = np.array([q, y[m-1]])
+            j+=1
+        
+    return output
